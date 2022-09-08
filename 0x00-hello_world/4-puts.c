7828801aaf0815d7631455fb63502f370335adbf
John Ngethe <jngethe07@gmail.com>
2022-09-08 11:28:28 -0700

    betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,30 +1,20 @@
 /*
- *File 4-puts.c
- *Auth: John Ngethe
- */
+ *  * File: 4-puts.c
+ *   * Auth: Jefferson Tumuti
+ *    */
+
 #include <stdio.h>
 
-
-
-/*
- *
- *  main - Prints "Programming is like building a multilingual
- *
- * puzzle, followed by a new line.
- *
- * 
- *
- * Return: Always 0.
- */
-
-
+/**
+ *  * main - Prints "Programming is like building a multilingual
+ *   *                puzzle, followed by a new line.
+ *    *
+ *     * Return: Always 0.
+ *      */
 
 int main(void)
-
 {
+		puts("\"Programming is like building a multilingual puzzle");
 
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
-
+			return (0);
 }
-

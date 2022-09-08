f1d28358de604b53b4cb107c82144de152ca6c69
John Ngethe <jngethe07@gmail.com>
2022-09-08 13:42:57 -0700

    All complete

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main - prints out a main via printf
+ * Description: 'with proper grammar, but the outcome is a piece of art'
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-/*
+/**
  * main - Prints the size of various types based on
  *        the computer it is compiled and run on..
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {

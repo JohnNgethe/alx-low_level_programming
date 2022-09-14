69a74a77e8cbc15c39bc0f412cbc807aae146faf
John Ngethe <jngethe07@gmail.com>
2022-09-14 04:52:10 -0700

    C functions  and nest

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * main -retun lowercade followed by alphabet
- */
+ *  print_alphabet - prints the alphabet in lowercase followed by a new line.
+*/
 void print_alphabet(void)
 {
 char letter;

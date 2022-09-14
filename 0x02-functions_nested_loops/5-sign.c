23e14d8bfc0b93c671b9243fb0d4d1893ca8796f
John Ngethe <jngethe07@gmail.com>
2022-09-14 04:47:50 -0700

    c func and nest

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,10 +1,9 @@
-nclude "main.h"
-
+#include "main.h"
 /**
  * print_sign - prints the sign of a number.
  * @n: The number being checked.
  * Return: 1 and print '+' if n is greater than zero,
- *0 and print '0' if n is zero,
+ * 0  and print '0' if n is zero,
  *-1 and print '-' if n is less than zero.
  */
 int print_sign(int n)
